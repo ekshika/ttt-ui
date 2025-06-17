516065c3315178318ecd5bd964ffe995e5384528
Pawan <pawanxandromeda@gmail.com>
2025-06-17 11:59:53 -0700

    creds

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7070/api',
+  baseURL: 'https://ttt-backend-nb12.onrender.com/api',
   withCredentials: true, // so cookies (refresh token) are sent
   headers: { 'Content-Type': 'application/json' },
 });
